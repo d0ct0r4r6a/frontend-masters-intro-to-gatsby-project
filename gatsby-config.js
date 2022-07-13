b4df900f3c07b248f325d11a80163634ed0b0235
Arga Roh Sahrijal Saragih <arga.saragih@alumagubi.co.id>
2022-07-13 20:25:15 +0700

    Add site metadata

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://yourdomain.ltd',
+    title: 'Frontend Masters Intro',
+    description: 'Learning is fun!',
+    image: 'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg'
+  }
+}
