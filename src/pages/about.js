9604dad4d0edc598041d4a2c493094940275908f
Arga Roh Sahrijal Saragih <arga.saragih@alumagubi.co.id>
2022-07-13 18:11:36 +0700

    Create 2 basic pages

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>Hello, Frontend Masters Gatsby! This is the about page</h1>
+    </main>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default function IndexPage() {
+  return (
+    <main>
+      <h1>Hello, Frontend Masters Gatsby! Does hot reload work?</h1>
+    </main>
+  )
+}
