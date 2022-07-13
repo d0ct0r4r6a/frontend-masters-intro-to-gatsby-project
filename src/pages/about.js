b75602a0e4f571b6d34e622ffd6494b312cab1b7
Arga Roh Sahrijal Saragih <arga.saragih@alumagubi.co.id>
2022-07-13 20:25:05 +0700

    Add links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
+import { Link } from 'gatsby'
 import * as React from 'react'
 
 export default function AboutPage() {
   return (
     <main>
       <h1>Hello, Frontend Masters Gatsby! This is the about page</h1>
+      <Link to="/">Back to home</Link>
     </main>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
+import { Link } from 'gatsby'
 import * as React from 'react'
 
 export default function IndexPage() {
   return (
     <main>
       <h1>Hello, Frontend Masters Gatsby! Does hot reload work?</h1>
+      <Link to="/about">About page</Link>
     </main>
   )
 }
