86ba86a24a66daffddd1a6e203a8806173beee36
Arga Roh Sahrijal Saragih <arga.saragih@alumagubi.co.id>
2022-07-20 10:44:05 +0700

    Add Layout component, global and scoped styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,15 @@
 import { Link } from 'gatsby';
 import * as React from 'react';
-import { Seo } from '../components/seo';
+import Layout from '../components/layout';
 
 export default function AboutPage() {
   return (
-    <>
-      <Seo title="About this site" description="More information" />
-      <main>
-        <h1>Hello, Frontend Masters Gatsby! This is the about page</h1>
-        <Link to="/">Back to home</Link>
-      </main>
-    </>
+    <Layout
+      title="About This Site"
+      description="More information about this site"
+    >
+      <h1>Hello, Frontend Masters Gatsby! This is the about page</h1>
+      <Link to="/">Back to home</Link>
+    </Layout>
   );
 }
